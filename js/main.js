c34bf35d91fa3c304fc12898248d2d8f0e384919
Noe Kurzawa <noekurzawa@gmail.com>
2020-02-25 23:56:48 +0100

    section home work on section slick and sliders

--- js/main.js
+++ js/main.js
@@ -6,4 +6,37 @@ $(document).ready(function () {
             $(".navbar-toggler").click();
         }
     });
+
+    // slick.js
+  $(".global-carousel").slick({
+    // setting-name: setting-value
+    autoplay: true,
+    autoplaySpeed: 4500,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4
+        }
+      }
+    ]
+  });
 });
