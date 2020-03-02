36e877ed03dfa9a8904a84c2a3161dfd2029d21d
Noe Kurzawa <noekurzawa@gmail.com>
2020-03-02 22:28:28 +0100

    change

--- js/main.js
+++ js/main.js
@@ -15,28 +15,5 @@ $(document).ready(function () {
     mobileFirst: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    responsive: [
-      
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-
-      {
-        breakpoint: 1600,
-        settings: {
-          slidesToShow: 4
-        }
-      }
-    ]
   });
 });
