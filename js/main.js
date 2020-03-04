bc2d5cb8d4eb1e0925b6127e35cf56954f111c79
Noe Kurzawa <noekurzawa@gmail.com>
2020-03-04 23:14:22 +0100

    added animations in 2 section/start about-info. scroll-fix ready to use. acordeon.  about-hover added plus visibility:hidden

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("show");
-        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-            $(".navbar-toggler").click();
-        }
+$(document).ready(function() {
+    $(document).click(function(event) {
+      var clickover = $(event.target);
+      var _opened = $(".navbar-collapse").hasClass("show");
+      if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+        $(".navbar-toggler").click();
+      }
     });
-});
+  });
